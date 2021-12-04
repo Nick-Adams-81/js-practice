7a0cbc4fbb593e6afce2e281fca92329376e2552
nick adams <thebigdeal916@icloud.com>
2021-12-04 10:18:12 -0600

    added fizzbuzz

--- practice.js
+++ practice.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log("hello");
+
+//Fizz-buzz
+
+for(let i = 1; i < 50; i++) {
+    if(i % 15 === 0) {
+        console.log("fizz-buzz!")
+    } else if(i % 3 === 0) {
+        console.log("Fizz")
+    } else if(i % 5 === 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
